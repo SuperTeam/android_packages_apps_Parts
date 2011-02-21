f9c429e8f61501a54e298b641d59e80f156fc080
macsa <y>
2011-02-21 16:19:42 +0100

    Fix Widgets
    Change-Id: I91cf80cd243c758b8a01b94c39dd3cf2047841ce

--- src/com/cyanogenmod/cmparts/utils/PowerWidgetUtil.java
+++ src/com/cyanogenmod/cmparts/utils/PowerWidgetUtil.java
@@ -94,9 +94,7 @@ public class PowerWidgetUtil {
     private static final String BUTTONS_DEFAULT = BUTTON_WIFI
                              + BUTTON_DELIMITER + BUTTON_BLUETOOTH
                              + BUTTON_DELIMITER + BUTTON_GPS
-                             + BUTTON_DELIMITER + BUTTON_SOUND
-                             + BUTTON_DELIMITER + BUTTON_MOBILEDATA
-                             + BUTTON_DELIMITER + BUTTON_AUTOROTATE;
+                             + BUTTON_DELIMITER + BUTTON_SOUND;
 
     public static String getCurrentButtons(Context context) {
         String buttons = Settings.System.getString(context.getContentResolver(), Settings.System.WIDGET_BUTTONS);
