0a56496671d75bff9bfed1f1384aaa1e4bcfc622
podxboq <podxboq@gmail.com>
2011-10-13 00:02:31 +0200

    Clean code

--- src/com/cyanogenmod/cmparts/activities/UIActivity.java
+++ src/com/cyanogenmod/cmparts/activities/UIActivity.java
@@ -17,10 +17,6 @@
 package com.cyanogenmod.cmparts.activities;
 
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.Parcel;
-import android.os.RemoteException;
-import android.os.ServiceManager;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
